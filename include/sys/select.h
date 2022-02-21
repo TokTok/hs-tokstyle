864d95f714bbe273c2cdebf833d1ba782baea2bd
Restyled.io <commits@restyled.io>
2022-02-21 10:38:21 +0000

    Restyled by clang-format

--- include/sys/select.h
+++ include/sys/select.h
@@ -7,4 +7,5 @@ typedef struct fd_set fd_set;
 void FD_ZERO(fd_set *set);
 void FD_SET(int fd, fd_set *set);
 
-int select(int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds, struct timeval *timeout);
+int select(int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds,
+           struct timeval *timeout);
--- include/sys/time.h
+++ include/sys/time.h
@@ -3,19 +3,19 @@
 #include "../time.h"
 
 typedef enum {
-    CLOCK_MONOTONIC = 0,
+  CLOCK_MONOTONIC = 0,
 } clockid_t;
 
 typedef long suseconds_t;
 
 struct timeval {
-    time_t tv_sec;
-    suseconds_t tv_usec;
+  time_t tv_sec;
+  suseconds_t tv_usec;
 };
 
 struct timespec {
-    time_t tv_sec;
-    long tv_nsec;
+  time_t tv_sec;
+  long tv_nsec;
 };
 
 int clock_gettime(clockid_t clk_id, struct timespec *tp);
