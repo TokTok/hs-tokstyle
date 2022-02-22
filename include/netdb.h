7fc71b26c3913a2707e5f737642c8273d2e1ff9d
Restyled.io <commits@restyled.io>
2022-02-22 01:41:16 +0000

    Restyled by astyle

--- include/netdb.h
+++ include/netdb.h
@@ -1,11 +1,11 @@
 #pragma once
 
 struct addrinfo {
-  int ai_family;
-  int ai_socktype;
-  struct sockaddr *ai_addr;
-  int ai_addrlen;
-  struct addrinfo *ai_next;
+    int ai_family;
+    int ai_socktype;
+    struct sockaddr *ai_addr;
+    int ai_addrlen;
+    struct addrinfo *ai_next;
 };
 
 int getaddrinfo(const char *node, const char *service,
